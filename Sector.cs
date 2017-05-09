9a33e5c72241fb9365cfb6c2e2bdd094a845d4b8
Marko Harjula <marko.harjula@gmail.com>
2017-05-09 08:34:48 +0300

    Create Sector.cs

---
+++ Sector.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class Sector {
+	public TerrainData terrainData;
+	public int offsetX;
+	public int offsetY;
+	public Sector(int x,int y) 
+	{
+
+	}
+}
